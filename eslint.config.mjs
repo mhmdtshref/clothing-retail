7f29e4030bdf31f036ce88673fda04b1164b7c6c
Mohamed Sharif <mhmdtshref@gmail.com>
2025-10-24 19:08:34 +0300

    chore: init Next.js (App Router) with ESLint + Prettier + SCSS (JS)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  prettierRecommended,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+  {
+    rules: {
+      'prettier/prettier': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
 ]);
 
 export default eslintConfig;
