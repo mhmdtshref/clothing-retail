d441fc9d456257c3967725b3c70358e454d15bc8
Mohamed Sharif <mhmdtshref@gmail.com>
2025-11-14 17:31:25 +0200

    setup pwa for windows/mobiles

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 const PROD_URL = process.env.NEXT_PUBLIC_APP_URL;
 
 const config: CapacitorConfig = {
-  appId: 'ai.imenu.lariche',
-  appName: 'Lariche Boutique POS',
+  appId: '[APP_ID_HERE-ex: com.example.app]',
+  appName: 'APP_NAME_HERE',
   webDir: 'out',
   bundledWebRuntime: false,
   server: {
